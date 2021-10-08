4b1f9f008bb4514eebaf631dfb78a47a08bd7716
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 08:28:21 +0900

    セルの初期化設定など

--- SinatraPractice/SinatraPractice/PrefecturesTableViewCell.swift
+++ SinatraPractice/SinatraPractice/PrefecturesTableViewCell.swift
@@ -7,17 +7,20 @@
 
 import UIKit
 
-class PrefecturesTableViewCell: UITableViewCell {
+final class PrefecturesTableViewCell: UITableViewCell {
+
+    @IBOutlet private weak var nameLabel: UILabel!
+    
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configure(name: String) {
+        nameLabel.text = name
     }
     
 }
