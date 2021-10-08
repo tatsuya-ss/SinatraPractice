5c77eec67521dd96b6c02f2c1e675a3641a75375
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 08:27:49 +0900

    Cellのxib作成

---
+++ SinatraPractice/SinatraPractice/PrefecturesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PrefecturesTableViewCell.swift
+//  SinatraPractice
+//
+//  Created by 坂本龍哉 on 2021/10/09.
+//
+
+import UIKit
+
+class PrefecturesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
