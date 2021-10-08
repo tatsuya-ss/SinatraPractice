eb0af7af93da6ad897756b94956062409531026b
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 08:28:52 +0900

    TableViewの設定

--- SinatraPractice/SinatraPractice/ViewController.swift
+++ SinatraPractice/SinatraPractice/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private var prefectures: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.register(PrefecturesTableViewCell.nib,
+                           forCellReuseIdentifier: PrefecturesTableViewCell.identifier)
     }
 
+}
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        prefectures.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PrefecturesTableViewCell.identifier,
+                                                       for: indexPath) as? PrefecturesTableViewCell
+        else { fatalError("セルが見つかりません") }
+        cell.configure(name: prefectures[indexPath.row])
+        return cell
+    }
+        
 }
 
