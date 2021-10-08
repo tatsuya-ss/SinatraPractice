d4469f443e6f8dc1e042fcaa0103d80e602babb0
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 08:45:40 +0900

    ruby myapp.rbを実行中のみ都道府県を取得する処理作成

--- SinatraPractice/SinatraPractice/ViewController.swift
+++ SinatraPractice/SinatraPractice/ViewController.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 
+enum FetchError: Error {
+    case urlError
+    case responseError
+}
+
+struct Prefectures: Decodable {
+    var prefectures: [String]
+}
+
 final class ViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
@@ -16,12 +25,47 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        fetchPrefectures { [weak self] result in
+            switch result {
+            case .success(let prefectures):
+                self?.prefectures = prefectures.prefectures
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
     private func setupTableView() {
         tableView.dataSource = self
         tableView.register(PrefecturesTableViewCell.nib,
                            forCellReuseIdentifier: PrefecturesTableViewCell.identifier)
+    }
+    
+    private func fetchPrefectures(completion: @escaping (Result<Prefectures, Error>) -> Void) {
+        let localhostURLString = "http://localhost:4567/prefectures"
+        guard let url = URL(string: localhostURLString) else {
+            completion(.failure(FetchError.urlError))
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            guard let data = data else {
+                completion(.failure(FetchError.responseError))
+                return
+            }
+            do {
+                let data = try JSONDecoder().decode(Prefectures.self, from: data)
+                completion(.success(data))
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
     }
 
 }
